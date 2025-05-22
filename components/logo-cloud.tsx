fe2cc356078c911fa73bdbba51fe4b4c22be1a13
Logipek <hugoguttr@gmail.com>
2025-05-22 19:14:07 +0200

    Amélioration de la gestion des erreurs dans le composant de personnalisation, ajout de vérifications pour les informations de suivi des commandes et mise à jour des redirections dans la page des détails de commande. Optimisation de l'affichage des produits et ajustements divers pour une meilleure expérience utilisateur.

---
+++ components/logo-cloud.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+// Ce composant est un alias pour BrandsSlider
+import { BrandsSlider } from './brands-slider'
+
+// Export par défaut pour corriger l'importation dans home-content
+export default BrandsSlider
