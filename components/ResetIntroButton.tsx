4f7ab3cea22f7410e628cfb4de3af23a7726b2c4
Logipek <hugoguttr@gmail.com>
2025-06-14 13:33:27 +0200

    Suppression du bouton de réinitialisation de l'introduction dans le composant ResetIntroButton. Le composant retourne désormais un fragment vide

--- components/ResetIntroButton.tsx
+++ components/ResetIntroButton.tsx
@@ -9,12 +9,6 @@ export default function ResetIntroButton() {
   };
 
   return (
-    <button
-      onClick={resetIntro}
-      className="fixed bottom-4 right-4 z-[9998] px-3 py-2 bg-red-600 text-white text-sm rounded opacity-50 hover:opacity-100 transition-opacity"
-      title="Reset intro (dev only)"
-    >
-      Reset Intro
-    </button>
+  <></>
   );
 }
